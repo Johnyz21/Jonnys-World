1433ea18fa7f746cdec58eeb2f76431ed7362a33
Jonathan Markland <jonathan.markland@mettle.co.uk>
2022-01-02 14:53:27 +0000

    Added google-analytics

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import VueGtag from 'vue-gtag'
 
 Vue.config.productionTip = false
+Vue.use(VueGtag, {
+  config: { id: 'UA-112821703-1' }
+})
 
 new Vue({
   router,
